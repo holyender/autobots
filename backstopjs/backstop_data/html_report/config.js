d143839544dde14f9892d887c4c8d556e882187e
Ubuntu <holyender@autobot.qcqvjwpols0elgzoquha4lg50h.bx.internal.cloudapp.net>
2018-06-26 16:18:27 +0000

    introducing backstopjs to test suite. jenkins function coming

---
+++ backstopjs/backstop_data/html_report/config.js
@@ -0,0 +1,54 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_BackstopJS_Homepage_0_document_0_compscreen.png",
+        "test": "../bitmaps_test/20180626-161310/backstop_default_BackstopJS_Homepage_0_document_0_compscreen.png",
+        "selector": "document",
+        "fileName": "backstop_default_BackstopJS_Homepage_0_document_0_compscreen.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://www.911memorial.org/getting-here",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "compscreen",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_interactive-911-timelines_0_document_0_compscreen.png",
+        "test": "../bitmaps_test/20180626-161310/backstop_default_interactive-911-timelines_0_document_0_compscreen.png",
+        "selector": "document",
+        "fileName": "backstop_default_interactive-911-timelines_0_document_0_compscreen.png",
+        "label": "interactive-911-timelines",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://www.911memorial.org/interactive-911-timelines",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "compscreen",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
