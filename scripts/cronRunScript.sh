8d1849a91c48baf040e37e224114177db2950191
Bill Liu <bliu@911MEMORIAL.ORG>
2018-07-06 10:22:46 -0400

    c

--- scripts/cronRunScript.sh
+++ scripts/cronRunScript.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+echo"cron job start"
 xvfb-run java -Dwebdriver.chrom.driver=/usr/local/bin/chromedriver -jar /usr/local/bin/selenium-server-standalone.jar &
 
 #cd ~
 #cd autobots
 #git pull
 #cd test2
+echo "codecept test start"
 cd /home/bliu/autobots/test2/
 /home/bliu/codecept/vendor/bin/codecept run --steps --html
 echo "codecept test finished"
