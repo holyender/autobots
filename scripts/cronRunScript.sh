d8c9c6d18248a7d5244dabd02e808c7e2dd6ad58
Bill Liu <bliu@911MEMORIAL.ORG>
2018-07-09 11:23:05 -0400

    cron

--- scripts/cronRunScript.sh
+++ scripts/cronRunScript.sh
@@ -19,6 +19,7 @@ cd /home/bliu/autobots/backstopjs
 sudo /usr/bin/backstop test
 echo "backstop test finished"
 
+mutt -s "Subject" -a /home/bliu/autobots/test2/tests/_output/report.html -- bliu@911memorial.org
 mutt -s "Subject" -a /home/bliu/autobots/test2/tests/_output/report.html -a /home/bliu/autobots/backstopjs/backstop_data/html_report/index.html -- bliu@911memorial.org <<< 'Report for __ Date __ Time __ Pass Fail'
 
 echo "\n mail sent cron run complete"
