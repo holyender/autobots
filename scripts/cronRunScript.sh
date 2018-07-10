f09bd63978ab92bb91363e249a21d32dc893f9e2
Bill Liu <bliu@911MEMORIAL.ORG>
2018-07-10 15:57:27 -0400

    cron push to website

--- scripts/cronRunScript.sh
+++ scripts/cronRunScript.sh
@@ -22,15 +22,19 @@ echo "backstop test finished"
 
 cd /home/bliu
 
-zip -r backstop_report.zip autobots/backstopjs/backstop_data/html_report/
+#zip -r backstop_report.zip autobots/backstopjs/backstop_data/html_report/
 
 #mutt -s "Report!" -a /home/bliu/autobots/test2/tests/_output/report.html -- rnagaraja@911memorial.org < /home/bliu/autobots/scraps/runGiftTestCest.sh
 
 #mutt -s "Report!Report!" -a /home/bliu/autobots/test2/tests/_output/report.html -- bliu@911memorial.org < /home/bliu/autobots/scraps/runGiftTestCest.sh
 
-mutt -s "Report!" -a /home/bliu/autobots/test2/tests/_output/report.html -a /home/bliu/backstop_report.zip -- rnagaraja@911memorial.org
-mutt -s "Subject" -a /home/bliu/autobots/test2/tests/_output/report.html -a /home/bliu/backstop_report.zip -- bliu@911memorial.org
+#mutt -s "Report!" -a /home/bliu/autobots/test2/tests/_output/report.html -a /home/bliu/backstop_report.zip -- rnagaraja@911memorial.org
+#mutt -s "Subject" -a /home/bliu/autobots/test2/tests/_output/report.html -a /home/bliu/backstop_report.zip -- bliu@911memorial.org
 
+sudo cp /home/bliu/autobots/test2/tests/_output/report.html /var/www/html/
 
+sudp cp /home/bliu/autobots/backstopjs/backstop_data/ci_report/xunit.xml /var/www/html/
+
+sudo cp -R /home/bliu/autobots/backstopjs/backstop_data /var/www/html/
 
 echo "\n mail sent cron run complete"
